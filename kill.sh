06a61539f64fbeeb86faaf7202103da7f40fc04b
Wang <nkhs@yandex.com>
2020-02-13 22:54:09 +0000

    234

--- kill.sh
+++ kill.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 user=$1
 portFile=/opt/${user}/port.txt
-if [ -e /opt/user1/port.txt ]
-then
-    value=`cat ${portFile}`
-    echo "${value}"
-else
-    echo "nok${portFile}"
-fi
+value=`cat ${portFile}`
+echo "${value}"
+lsof -i:${value}
