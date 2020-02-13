d29d9ee41c65ad7faa70924bcbe701f88dd015d6
unknown <nkhs@yandex.com>
2020-02-14 04:19:18 +0800

    init

---
+++ config.js
@@ -0,0 +1,7 @@
+
+var host_url = '167.99.199.12';
+
+module.exports = {
+  host_url: host_url,
+  WEB_SOURCE: './public/dist',
+};
---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+var fs = require('fs');
+const path = require("path");
+
+const bodyParser = require('body-parser');
+const config = require('./config');
+var watch = require('node-watch');
+const cors = require('cors');
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({
+  limit: '5mb',
+  extended: true
+}));
+app.use(bodyParser.json({
+  limit: '5mb'
+}));
+
+app.use(cors());
+app.use(function (err, req, res, next) {
+  if (err.name === 'StatusError') {
+    res.send(err.status, err.message);
+  } else {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next(err);
+  }
+});
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({
+  server
+});
+
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        // console.log("sending data " + data);
+        client.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+};
+var argUser = process.argv.slice(2);
+
+user = 'user1';
+if (argUser != null && argUser.length > 0) user = argUser[0];
+var DIR = `opt/${user}`;
+
+watch(DIR, { recursive: false }, function (evt, name) {
+  var content = fs.readFileSync(name, 'utf8');
+  console.log('%s changed.', name, content);
+
+  wss.broadcast(JSON.stringify({ file: name, text: content }))
+});
+
+var normalizePort = (val) => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+var port = normalizePort(process.env.PORT || "8090");
+
+server.listen(port, function listening() {
+  console.log(`HTTP Listening on http://${config.host_url}:${server.address().port}`);
+});
