3d170317983a499881fc928308dcc366125e9831
Wang <nkhs@yandex.com>
2020-02-13 23:00:29 +0000

    234

--- kill.sh
+++ kill.sh
@@ -3,4 +3,8 @@ user=$1
 portFile=/opt/${user}/port.txt
 value=`cat ${portFile}`
 echo "Kill port ${value}"
-kill -9 $(lsof -t -i:${value})
+if kill -9 $(lsof -t -i:${value}); then
+    echo 'success'
+else
+    echo 'already killed'
+fi
